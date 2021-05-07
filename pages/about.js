4a57d40bd98c365e9566c67e1fa123576bb33184
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-05-07 03:44:03 +0300

    use image with next

--- pages/about.js
+++ pages/about.js
@@ -6,7 +6,13 @@ const about = () => {
       <div>
         <Link href="/">go to home</Link>
       </div>
-      Hi, I am react developer
+      <p>Hi, I am react developer</p>
+      <img
+        alt="react-logo"
+        src="/static/react-logo.png"
+        height="80%"
+        width="100%"
+      />
     </div>
   );
 };
