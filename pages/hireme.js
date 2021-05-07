59bcdd3e59dba3234452e1f6f6e2dfdf68232ce4
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-05-07 15:07:09 +0300

    create hireme page

---
+++ pages/hireme.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const hireme = ({ children, title }) => {
+  return (
+    <Layout title="Hire Me">
+      <a href="milato:mohjbe.dev@gmail.com">mohjbe.dev@gmail.com</a>
+    </Layout>
+  );
+};
+
+export default hireme;
